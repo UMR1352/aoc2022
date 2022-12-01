264d4a443f984566659653cdb075e389afb0108e
Enrico Marconi <enrico.marconi@skillbill.it>
2022-12-01 09:34:09 +0100

    Day1 improvement

--- src/day1.rs
+++ src/day1.rs
@@ -1,27 +1,25 @@
 use itertools::Itertools;
 
 #[aoc_generator(day1)]
-fn gerator(input: &str) -> Vec<Vec<u32>> {
+fn gerator(input: &str) -> Vec<u32> {
     input
         .split("\n\n")
-        .map(|s| s.lines().flat_map(str::parse).collect())
+        .map(|s| s.lines().flat_map(str::parse::<u32>).sum())
         .collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(input: &[Vec<u32>]) -> u32 {
-    input
+fn part1(input: &[u32]) -> u32 {
+    *input
         .iter()
-        .map(|cals| cals.into_iter().sum())
         .max()
         .unwrap()
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &[Vec<u32>]) -> u32 {
+fn part2(input: &[u32]) -> u32 {
     input
         .iter()
-        .map(|cals| cals.into_iter().sum::<u32>())
         .sorted()
         .rev()
         .take(3)
