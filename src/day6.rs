ef81defe22334e536f548cc1c25779876b2063ec
Enrico Marconi <enrico.marconi@skillbill.it>
2022-12-06 08:58:22 +0100

    Day 6: performance improvements

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,23 @@
-use itertools::Itertools;
+trait AllUniqueIter: Iterator {
+    fn all_unique(self) -> bool;
+}
+
+impl<'a, I: Iterator<Item = &'a u8>> AllUniqueIter for I {
+    fn all_unique(mut self) -> bool {
+        let mut set = [0_u64; 4];
+        while let Some(byte) = self.next() {
+            let slot = (byte / 64) as usize;
+            let offset = (byte % 64) as usize;
+            if set[slot] & (1 << offset) != 0 {
+                return false;
+            } else {
+                set[slot] |= 1 << offset;
+            }
+        }
+
+        true
+    }
+}
 
 #[aoc(day6, part1)]
 fn part1(input: &[u8]) -> usize {
